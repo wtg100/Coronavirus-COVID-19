66639a57c617adb6812abd89167214bf3d66dd1e
Evandro Silva Pinto <evandrosilvapinto@outlook.com>
2020-03-18 18:16:19 -0400

    Update api.ts

--- src/app/shared/models/api.ts
+++ src/app/shared/models/api.ts
@@ -1,10 +1,11 @@
-export interface Api {
-    timelineitems: [],
+export interface Api  {
+  data: Api_data[]
+}
+export interface Api_data {
     new_daily_cases: number,
     new_daily_deaths: number,
     total_cases: number,
     total_recoveries: number,
     total_deaths: number;
 }
-
 // "1/23/2020":{}
