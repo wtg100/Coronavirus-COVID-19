63b187069ed13f1864f434f3beb6003a4dce3a74
wtg100 <wtg100@gmail.com>
2020-03-19 00:19:11 -0400

    Update api.ts

--- src/app/shared/models/api.ts
+++ src/app/shared/models/api.ts
@@ -1,11 +1,8 @@
-export interface Api  {
-  data: Api_data[]
+export class Api_data {
+  data: string;
+  new_daily_cases: number;
+  new_daily_deaths: number;
+  total_cases: number;
+  total_recoveries: number;
+  total_deaths: number;
 }
-export interface Api_data {
-    new_daily_cases: number,
-    new_daily_deaths: number,
-    total_cases: number,
-    total_recoveries: number,
-    total_deaths: number;
-}
-// "1/23/2020":{}
